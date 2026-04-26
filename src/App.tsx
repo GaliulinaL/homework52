f33b2b4b93e0e9726bf113f132104e102b803986
Leila <galiul1naaal@gmail.com>
2026-04-27 00:08:16 +0600

    add card classes and structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+      <div className={"playingCards faceImages"}>
+  <span className="card rank-k diams">
+    <span className="rank">K</span>
+    <span className="suit">♦</span>
+  </span>
+      </div>
+  )
+};
+
+export default App
